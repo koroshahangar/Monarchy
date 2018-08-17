16105c13a53d553b4a0d05eecaa53ddd14603be6
koroshahangar <ahangar.korosh@gmail.com>
2018-08-17 19:09:32 -0400

    Provide the missing namespace specifier for operator<< for Position

--- src/Position.cc
+++ src/Position.cc
@@ -2,7 +2,7 @@
 
 using namespace Monarchy;
 
-std::ostream& operator<<(std::ostream& os, const Position position) {
+std::ostream& Monarchy::operator<<(std::ostream& os, const Position position) {
 	return os << "Position: (" 
 		<< position.getX() << ", " 
 		<< position.getY() << ")" 
