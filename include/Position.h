b12f1318ea0af7a2673e89c7afea87543cfa374b
koroshahangar <ahangar.korosh@gmail.com>
2018-08-17 19:29:56 -0400

    Make the coordinate members of Position public and so can be accessed directly

--- include/Position.h
+++ include/Position.h
@@ -8,14 +8,12 @@ namespace Monarchy {
 	typedef unsigned short Coordinate;
 
 	class Position
-	{
-	private:
+	{		
+	public:
 		Coordinate x;
 		Coordinate y;
-	public:
-		Position(Coordinate x = 0, Coordinate y = 0): x(x), y(y) {};
-		const Coordinate getX() const { return x; }
-		const Coordinate getY() const { return y; }
+
+		Position(Coordinate x = 0, Coordinate y = 0): x(x), y(y) {};		
 	};
 	std::ostream& operator<<(std::ostream& os, const Position position);
 	
--- src/Position.cc
+++ src/Position.cc
@@ -4,7 +4,7 @@ using namespace Monarchy;
 
 std::ostream& Monarchy::operator<<(std::ostream& os, const Position position) {
 	return os << "Position: (" 
-		<< position.getX() << ", " 
-		<< position.getY() << ")" 
+		<< position.x << ", " 
+		<< position.y << ")" 
 		<< std::endl ;
 }
