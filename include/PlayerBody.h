55453b165220cb90057c2b54b317081c405ec4e2
koroshahangar <ahangar.korosh@gmail.com>
2018-08-17 21:43:54 -0400

    Add a team id to PlayerBody

--- include/PlayerBody.h
+++ include/PlayerBody.h
@@ -8,6 +8,7 @@ namespace Monarchy {
 
 	typedef unsigned short BloodLevel;
 	typedef unsigned short UnitId;
+	typedef unsigned short TeamId;
 	enum class UnitType { Leader, Archer, Spearman };
 	std::ostream& operator<<(std::ostream& os, const UnitType& unit_type);
 
@@ -18,12 +19,14 @@ namespace Monarchy {
 		Position position;	
 		UnitType unit_type;
 		UnitId unit_id;
+		TeamId team_id;
 	public:
 		PlayerBody(BloodLevel blood, Position position, UnitType type, UnitId id);
 		const BloodLevel getBlood() const { return blood; }
 		const Position getPosition() const { return position; }
 		const UnitType getUnitType() const { return unit_type; }
 		const UnitId getUnitId() const { return unit_id; }
+		const UnitId getTeamId() const { return team_id; }
 
 	};
 	std::ostream& operator<<(std::ostream& os, const PlayerBody& agent);
