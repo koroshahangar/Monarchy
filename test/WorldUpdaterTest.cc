70591763d32dc61a27e92e4a6a50f62eb6da9b97
koroshahangar <ahangar.korosh@gmail.com>
2018-09-27 10:23:47 -0400

    Use a fixture to test WorldUpdater

--- test/WorldUpdaterTest.cc
+++ test/WorldUpdaterTest.cc
@@ -3,106 +3,91 @@
 
 using namespace Monarchy;
 
-TEST(WorldUpdater, WorldUpdaterHandlesPlayerReproduction) {
+class WorldUpdaterTest : public ::testing::Test {
+  protected:
+    void SetUp() override {
+        updater = new WorldUpdater(world);
+        string team_name = "New Team Name";
+        TeamId team_id = world.getNewTeamId();
+        world.addTeam(std::make_unique<Team>(team_name, team_id));
+        team_name = "New Second Team Name";
+        team_id = world.getNewTeamId();
+        world.addTeam(std::make_unique<Team>(team_name, team_id));
+    }
+    void TearDown() override {
+        delete updater;
+    }
     World world;
-    WorldUpdater updater(world);
+    WorldUpdater* updater;
+    PlayerBody& getLeader1() {
+        return world.getPlayerBodies().begin()->second;
+    }
+    PlayerBody& getLeader2() {
+        return (world.getPlayerBodies().begin()++)->second;
+    }
+};
 
-    string team_name = "New Team Name";
-    TeamId team_id = world.getNewTeamId();
-    world.addTeam(std::make_unique<Team>(team_name, team_id));
-
-    PlayerBody& leader = world.getPlayerBodies().begin()->second;
+TEST_F(WorldUpdaterTest, WorldUpdaterHandlesPlayerReproduction) {
+    PlayerBody& leader1 = getLeader1();
     Position spawn_location(3,3);
-
     PlayerMovePtr move = std::make_unique<PlayerReproduction>(UnitType::Archer, spawn_location);
-
-    updater.handleMove(move, leader.getUnitId());
-
+    updater->handleMove(move, leader1.getUnitId());
     ASSERT_NE(world.getGameState().getUnitIdOfPlayerAt(spawn_location), 0);
 }
 
-TEST(WorldUpdater, WorldUpdaterHandlesPlayerWalk) {
-    World world;
-    WorldUpdater updater(world);
+TEST_F(WorldUpdaterTest, WorldUpdaterHandlesPlayerWalk) {
+    PlayerBody& leader1 = getLeader1();
 
-    string team_name = "New Team Name";
-    TeamId team_id = world.getNewTeamId();
-    world.addTeam(std::make_unique<Team>(team_name, team_id));
-
-    PlayerBody& body = world.getPlayerBodies().begin()->second;
-
-    Position current_position = body.getPosition();
+    Position current_position = leader1.getPosition();
     Position desitnation1 (current_position.x, current_position.y + 1);
 
     PlayerMovePtr move = std::make_unique<PlayerWalk>(desitnation1);
 
-    updater.handleMove(move, body.getUnitId());
+    updater->handleMove(move, leader1.getUnitId());
 
-    ASSERT_EQ(body.getPosition(), desitnation1);
+    ASSERT_EQ(leader1.getPosition(), desitnation1);
 
-    current_position = body.getPosition();
+    current_position = leader1.getPosition();
     Position desitnation2 (current_position.x + 1, current_position.y);
 
     move = std::make_unique<PlayerWalk>(desitnation2);
 
-    updater.handleMove(move, body.getUnitId());
+    updater->handleMove(move, leader1.getUnitId());
 
-    ASSERT_EQ(body.getPosition(), desitnation2);
+    ASSERT_EQ(leader1.getPosition(), desitnation2);
 
 }
 
-TEST(WorldUpdater, WorldUpdaterHandlesArrowAttack) {
-    World world;
-    WorldUpdater updater(world);
+TEST_F(WorldUpdaterTest, WorldUpdaterHandlesArrowAttack) {
+    PlayerBody& leader1 = getLeader1();
+    PlayerBody& leader2 = getLeader2();
 
-    string team_name = "New Team Name";
-    TeamId team_id = world.getNewTeamId();
-    world.addTeam(std::make_unique<Team>(team_name, team_id));
-
-    PlayerBody& body1 = world.getPlayerBodies().begin()->second;
-
-    team_name = "New Second Team Name";
-    team_id = world.getNewTeamId();
-    world.addTeam(std::make_unique<Team>(team_name, team_id));
-
-    PlayerBody& body2 = (world.getPlayerBodies().begin()++)->second;
-
-    while(abs(body2.getPosition().x - body1.getPosition().x) > 1) {
+    while(abs(leader2.getPosition().x - leader1.getPosition().x) > 1) {
         Position destination;
-        if(body2.getPosition().x > body1.getPosition().x)
-            destination = Position(body2.getPosition().x -1, body2.getPosition().y);
+        if(leader2.getPosition().x > leader1.getPosition().x)
+            destination = Position(leader2.getPosition().x -1, leader2.getPosition().y);
         else
-            destination = Position(body2.getPosition().x +1, body2.getPosition().y);
+            destination = Position(leader2.getPosition().x +1, leader2.getPosition().y);
         PlayerMovePtr move = std::make_unique<PlayerWalk>(destination);
-        updater.handleMove(move, body1.getUnitId());
+        updater->handleMove(move, leader1.getUnitId());
     }
-    while(abs(body2.getPosition().y - body1.getPosition().y) > 1) {
+    while(abs(leader2.getPosition().y - leader1.getPosition().y) > 1) {
         Position destination;
-        if(body2.getPosition().y > body1.getPosition().y)
-            destination = Position(body2.getPosition().x, body2.getPosition().y -1);
+        if(leader2.getPosition().y > leader1.getPosition().y)
+            destination = Position(leader2.getPosition().x, leader2.getPosition().y -1);
         else
-            destination = Position(body2.getPosition().x, body2.getPosition().y +1);
+            destination = Position(leader2.getPosition().x, leader2.getPosition().y +1);
         PlayerMovePtr move = std::make_unique<PlayerWalk>(destination);
-        updater.handleMove(move, body1.getUnitId());
+        updater->handleMove(move, leader1.getUnitId());
     }
 
-    BloodLevel before_attack = body2.getBlood();
+    BloodLevel before_attack = leader2.getBlood();
 
-    PlayerMovePtr move = std::make_unique<ArrowAttack>(body2.getUnitId());
-    updater.handleMove(move, body1.getUnitId());
+    PlayerMovePtr move = std::make_unique<ArrowAttack>(leader2.getUnitId());
+    updater->handleMove(move, leader1.getUnitId());
 
-    BloodLevel after_attack = body2.getBlood();
+    BloodLevel after_attack = leader2.getBlood();
 
     ASSERT_TRUE(after_attack < before_attack);
-    //ASSERT_EQ(body2.getBloodLevel(), desitnation1);
-    //
-    //current_position = body.getPosition();
-    //Position desitnation2 (current_position.x + 1, current_position.y);
-    //
-    //move = std::make_unique<PlayerWalk>(desitnation2);
-    //
-    //updater.handleMove(move, body.getUnitId());
-    //
-    //ASSERT_EQ(body.getPosition(), desitnation2);
 
 }
