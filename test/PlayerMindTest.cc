b8b05204464626e151d61f80dd899a054ff8ee5f
koroshahangar <ahangar.korosh@gmail.com>
2018-10-03 11:36:31 -0400

    Use a fixture to test PlayerMind

--- test/PlayerMindTest.cc
+++ test/PlayerMindTest.cc
@@ -3,55 +3,43 @@
 
 using namespace Monarchy;
 
-TEST(PlayerMind, ConstructorWorksInASimpleCase) {
+class PlayerMindTest : public ::testing::Test {
+  protected:
+    PlayerMindTest():
+        body(blood, position, UnitInfo {unit_id, unit_type, team_id}) {
 
-    // Player Body
-    BloodLevel blood = 15;
-    Position position(10, 15);
+    }
+
+    PlayerBody body;
+    BloodLevel blood {15};
+    Position position {10, 15};
     UnitType unit_type = UnitType::Leader;
     UnitId unit_id = 5;
     TeamId team_id = 1;
-    UnitInfo unit_info {unit_id, unit_type, team_id};
-    PlayerBody self(blood, position, unit_info);
+};
 
-    PlayerMind mind(self);
 
-    ASSERT_EQ(mind.getUnitId(), self.getUnitId());
+TEST_F(PlayerMindTest, ConstructorWorksInASimpleCase) {
+    PlayerMind mind(body);
+
+    ASSERT_EQ(mind.getUnitId(), body.getUnitId());
 }
 
-TEST(PlayerMind, EqaulityOperatorOnlyChecksBodyEquality) {
+TEST_F(PlayerMindTest, EqaulityOperatorOnlyChecksBodyEquality) {
 
-    // Player Body
-    BloodLevel blood = 15;
-    Position position(10, 15);
-    UnitType unit_type = UnitType::Leader;
-    UnitId unit_id = 5;
-    TeamId team_id = 1;
-    UnitInfo unit_info {unit_id, unit_type, team_id};
-    PlayerBody self(blood, position, unit_info);
-
-    PlayerMind mind1(self);
-    PlayerMind mind2(self);
+    PlayerMind mind1(body);
+    PlayerMind mind2(body);
 
     ASSERT_EQ(mind1, mind2);
 }
 
-TEST(PlayerMind, NotEqualOperatorOnlyChecksBodyEquality) {
+TEST_F(PlayerMindTest, NotEqualOperatorOnlyChecksBodyEquality) {
+    UnitId another_unit_id = unit_id + 1;
+    PlayerBody another_body(blood, position, UnitInfo{another_unit_id, unit_type, team_id});
 
-    // Player Body
-    BloodLevel blood = 15;
-    Position position(10, 15);
-    UnitType unit_type = UnitType::Leader;
-    UnitId unit_id1 = 5;
-    UnitId unit_id2 = 6;
-    TeamId team_id = 1;
-
-    PlayerBody self1(blood, position, UnitInfo{unit_id1, unit_type, team_id});
-    PlayerBody self2(blood, position, UnitInfo{unit_id2, unit_type, team_id});
-
-    PlayerMind mind1(self1);
-    PlayerMind mind2(self2);
-    PlayerMind mind3(self1);
+    PlayerMind mind1(body);
+    PlayerMind mind2(another_body);
+    PlayerMind mind3(body);
 
     ASSERT_NE(mind1, mind2);
     ASSERT_NE(mind3, mind2);
