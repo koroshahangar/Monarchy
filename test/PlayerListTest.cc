49fef072891ce1119a0192037af00e0db2d43ade
koroshahangar <ahangar.korosh@gmail.com>
2018-10-03 12:03:04 -0400

    Use a fixture to test PlayerList

--- test/PlayerListTest.cc
+++ test/PlayerListTest.cc
@@ -4,46 +4,35 @@
 
 using namespace Monarchy;
 
-TEST(PlayerList, PlayersBodiesCanBeAdded) {
-    PlayerList list;
+class PlayerListTest : public ::testing::Test {
+  protected:
+    PlayerListTest():
+        body(blood, position, UnitInfo {unit_id, unit_type, team_id}) {
 
-    // Player Body
-    BloodLevel blood = 15;
-    Position position(10, 15);
+    }
+
+    PlayerList list;
+    BloodLevel blood {15};
+    Position position {10, 15};
     UnitType unit_type = UnitType::Leader;
     UnitId unit_id = 5;
     TeamId team_id = 1;
     UnitInfo unit_info {unit_id, unit_type, team_id};
+    PlayerBody body {blood, position, unit_info};
+};
 
-    PlayerBody body_copy1(blood, position, unit_info);
-    PlayerBody body_copy2(blood, position, unit_info);
-
+TEST_F(PlayerListTest, PlayersBodiesCanBeAdded) {
+    PlayerList list;
     list.addPlayerBody(PlayerBody(blood, position, unit_info));
-
-    ASSERT_EQ(body_copy1, list.getPlayerBody(body_copy1.getUnitId()));
-
-    PlayerList list2;
-    list2.addPlayerBody(body_copy2);
-    ASSERT_EQ(body_copy1, list.getPlayerBody(body_copy1.getUnitId()));
+    ASSERT_EQ(body, list.getPlayerBody(unit_id));
 }
 
 
-TEST(PlayerList, PlayersMindsCanBeAdded) {
-    PlayerList list;
-
-    // Player Body
-    BloodLevel blood = 15;
-    Position position(10, 15);
-    UnitType unit_type = UnitType::Leader;
-    UnitId unit_id = 5;
-    TeamId team_id = 1;
-    UnitInfo unit_info {unit_id, unit_type, team_id};
-
-    PlayerBody body(blood, position, unit_info);
+TEST_F(PlayerListTest, PlayersMindsCanBeAdded) {
     list.addPlayerBody(PlayerBody(blood, position, unit_info));
 
     PlayerMindPtr mind = std::make_unique<PlayerMind>(list.getPlayerBody(unit_id));
     list.addPlayerMind(std::move(mind));
 
-    ASSERT_EQ(list.getPlayerMind(unit_id)->getUnitId(), body.getUnitId());
+    ASSERT_EQ(list.getPlayerMind(unit_id)->getUnitId(), list.getPlayerBody(unit_id).getUnitId());
 }
