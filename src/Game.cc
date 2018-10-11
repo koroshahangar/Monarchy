f1e8bfb41be292e3276e7c228b33a75057d3a6c8
koroshahangar <ahangar.korosh@gmail.com>
2018-10-11 16:18:06 -0400

    Use a more descriptive name for the player whose turn it is

--- src/Game.cc
+++ src/Game.cc
@@ -21,17 +21,17 @@ void Game::addTeam(TeamPtr team) {
 void Game::executeNextMove() {
     if(world.getPlayerMinds().size() == 0)
         return;
-    static auto turn = world.getPlayerMinds().begin(); // Whose turn is it?
-    if(turn == world.getPlayerMinds().end())
-        turn = world.getPlayerMinds().begin();
-    UnitId unit_id = turn->second->getUnitId();
-    PlayerMovePtr move = turn->second->makeAMove(world.getGameState());
+    static auto nextAgent = world.getPlayerMinds().begin(); // Whose turn is it?
+    if(nextAgent == world.getPlayerMinds().end())
+        nextAgent = world.getPlayerMinds().begin();
+    UnitId unit_id = nextAgent->second->getUnitId();
+    PlayerMovePtr move = nextAgent->second->makeAMove(world.getGameState());
     try {
         updater.handleMove(move, unit_id);
     } catch(std::exception) {
         std::cout << "GOT AN EXCEPTION" << std::endl;
     }
-    turn++;
+    nextAgent++;
     world.getGameState().turn_count++;
 }
 
