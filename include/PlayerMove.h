fccf7301d8c8788e71bd360a7d7d03f11240f9cb
Korosh Ahangar <ahangar.korosh@gmail.com>
2019-07-03 15:26:44 -0400

    Format PlayerMove.h file

--- include/PlayerMove.h
+++ include/PlayerMove.h
@@ -30,40 +30,40 @@ struct PlayerReproduction final: public PlayerMove {
     const Position spawn_location;
     const UnitType spawn_type;
     PlayerReproduction(const UnitType type, const Position position);
-    std::string getRepr() override final{
-      std::stringstream repr;
-      repr << "Reproduction onto " << spawn_location;
-      return repr.str();
+    std::string getRepr() override final {
+        std::stringstream repr;
+        repr << "Reproduction onto " << spawn_location;
+        return repr.str();
     }
 };
 struct PlayerWalk final: public PlayerMove {
   public:
     const Position destination;
     PlayerWalk(const Position destination);
-    std::string getRepr() override final{
-      std::stringstream repr;
-      repr << "PlayerWalk to " << destination;
-      return repr.str();
+    std::string getRepr() override final {
+        std::stringstream repr;
+        repr << "PlayerWalk to " << destination;
+        return repr.str();
     }
 };
 struct SpearAttack final: public PlayerMove {
   public:
     const UnitId target;
     SpearAttack(const UnitId target);
-    std::string getRepr() override final{
-      std::stringstream repr;
-      repr << "SpearAttack at " << target;
-      return repr.str();
+    std::string getRepr() override final {
+        std::stringstream repr;
+        repr << "SpearAttack at " << target;
+        return repr.str();
     }
 };
 struct ArrowAttack final: public PlayerMove {
   public:
     const UnitId target;
     ArrowAttack(const UnitId target);
-    std::string getRepr() override final{
-      std::stringstream repr;
-      repr << "ArrowAttack at " << target;
-      return repr.str();
+    std::string getRepr() override final {
+        std::stringstream repr;
+        repr << "ArrowAttack at " << target;
+        return repr.str();
     }
 };
 
